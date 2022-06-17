3c2f32e4ecf5868e9e647b53112aa1dd7b1f2f3e
Pratyaksh <imaginatorypratyaksh@gmail.com>
2022-06-17 14:22:00 +0530

    fix: :rocket: Welcome page intro banner added

--- client/src/components/pages/home/Welcome.jsx
+++ client/src/components/pages/home/Welcome.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import 'assets/css/Welcome.css'
 import Banner from './Banner'
-
 import BgVideo from 'assets/videos/videoplayback.webm'
+import BgAudio from 'assets/audio/sorry_lofi_hip_hop.mp3'
+
 export default function Welcome() {
   return (
     <div className="welcome-page-container">
@@ -12,7 +13,9 @@ export default function Welcome() {
             <source src={BgVideo}/>
             </video>
         </div>
+        <audio autoPlay loop src={BgAudio}></audio>
         <Banner/>
+        
     </div>
   )
 }
